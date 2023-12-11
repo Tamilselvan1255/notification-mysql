ce239dfe1d94fc099e904a17149996b10ba930a1
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-11 19:19:42 +0530

    port updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const notificationsRouter = require('./notification');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/v1/api', notificationsRouter);
 
