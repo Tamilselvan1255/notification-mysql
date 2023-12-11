1c398206658d65410d688a19c8cd5834889a5c8c
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-11 20:18:40 +0530

    unwanted codes removed

--- db.js
+++ db.js
@@ -32,25 +32,3 @@ db.getConnection((err, connection) => {
 });
 
 module.exports = db;
-
-
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE,
-//   });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error('MySQL connection error:', err);
-//   } else {
-//     console.log('Connected to MySQL database');
-//   }
-// });
-
-// module.exports = db;
