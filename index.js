82058ed4814dcfbfb918c855e6501994e7a0c3e8
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-11 17:26:11 +0530

    env updated

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const notificationsRouter = require('./notification');
 const app = express();
 const port = 3000;
 
-// Use the notifications router
 app.use('/v1/api', notificationsRouter);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET');
+    next();
+  });
+
+app.get('/health', (req, res) => {
+    const data = {
+      uptime: process.uptime(),
+      message: 'Ok',
+      date: new Date()
+    }
+  
+    res.status(200).send(data);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
