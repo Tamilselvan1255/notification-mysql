8f45f54d1237bcf37a9aed76cba428fdf567c8a4
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-12 19:18:22 +0530

    delete commented logs

--- notification.js
+++ notification.js
@@ -127,57 +127,6 @@ function queryAsync(sql) {
   });
 }
 
-// Get notification endpoint
-router.get('/get-notification', (req, res) => {
-  const { page = 1, limit = 10, sortField = 'NotificationId', sortOrder = 'asc', search = '' } = req.query;
-
-  const offset = (page - 1) * limit;
-  const sortDirection = sortOrder.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-
-  // Construct the WHERE clause for searching by title
-  const searchCondition = search ? `WHERE title LIKE '%${search}%'` : '';
-
-  const selectSql = `
-    SELECT * FROM notifications
-    ${searchCondition}
-    ORDER BY ${sortField} ${sortDirection}
-    LIMIT ${limit} OFFSET ${offset}
-  `;
-
-  db.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting database connection:', err.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    connection.query(selectSql, (errSelect, result) => {
-      if (errSelect) {
-        connection.release();
-        return res.status(500).send({ error: errSelect.message });
-      }
-
-      // Count total records without pagination
-      const totalCountSql = `SELECT COUNT(*) as totalCount FROM notifications ${searchCondition}`;
-      connection.query(totalCountSql, (errCount, resultCount) => {
-        connection.release();
-        if (errCount) {
-          return res.status(500).send({ error: errCount.message });
-        }
-
-        const totalCount = resultCount[0].totalCount;
-
-        const paginationInfo = {
-          totalCount,
-          currentPage: +page,
-          totalPages: Math.ceil(totalCount / limit),
-        };
-
-        res.status(200).json({ data: result, pagination: paginationInfo });
-      });
-    });
-  });
-});
-
 // Edit notification endpoint
 router.patch('/edit-notification/:NotificationId', (req, res) => {
   const { NotificationId } = req.params;
@@ -196,37 +145,6 @@ router.patch('/edit-notification/:NotificationId', (req, res) => {
     }
   });
 });
-
-// Delete notification endpoint
-// router.delete('/delete-notification/:NotificationId', async (req, res) => {
-//   try {
-//     const { NotificationId } = req.params;
-//     // Delete notification from local database
-//     const deleteSql = 'DELETE FROM notifications WHERE NotificationId=?';
-//     db.query(deleteSql, [NotificationId], async (err, result) => {
-//       if (err) {
-//         return res.status(500).send({ error: err.message });
-//       }
-//       if (result.affectedRows === 0) {
-//         return res.status(404).send({ message: 'No matching notification found' });
-//       }
-//       // Notification deleted successfully from local database, now delete from external API
-//       try {
-//         const externalApiUrl = `https://app.nativenotify.com/api/notification/inbox/notification/16351/hYNQ78ihflsQqOQA5RhYBN/${NotificationId}`;
-//         const externalApiResponse = await axios.delete(externalApiUrl);
-//         // Handle the response from the external API
-//         console.log('External API Response:', externalApiResponse.data);
-//         res.status(200).send({ message: 'Notification deleted successfully' });
-//       } catch (externalApiError) {
-//         console.error('Error calling external API:', externalApiError);
-//         res.status(500).send({ error: 'Error calling external API' });
-//       }
-//     });
-//   } catch (error) {
-//     console.error('Error in delete-notification endpoint:', error);
-//     res.status(500).send({ error: 'Internal Server Error' });
-//   }
-// });
 
 // Delete notification endpoint
 router.delete('/delete-push-notification/:refer_notification_id', async (req, res) => {
