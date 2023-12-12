2efcef5d30ba3afe5c8cfbc9ae50819f62b046c4
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-12 13:50:08 +0530

    delete push notification added

--- notification.js
+++ notification.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
+const axios = require('axios');
 
 const router = express.Router();
 
@@ -14,18 +15,35 @@ router.use(bodyParser.urlencoded({ extended: true }));
 // Send notification endpoint
 router.post('/send-notification', async (req, res) => {
   try {
-    const { title, message, image, linkto } = req.body;
-
+    const { title, message, image, link } = req.body;
     // Send the base64-encoded image directly
     const base64Image = image;
-
-    const insertSql = 'INSERT INTO notifications (title, message, image, linkto) VALUES (?, ?, ?, ?)';
-    db.query(insertSql, [title, message, base64Image, linkto], (err, result) => {
+    // Insert notification into local database
+    const insertSql = 'INSERT INTO notifications (title, message, image, link) VALUES (?, ?, ?, ?)';
+    db.query(insertSql, [title, message, base64Image, link], async (err, result) => {
       if (err) {
         console.error('Error in send-notification endpoint:', err);
-        res.status(500).send({ error: err.message });
-      } else {
+        return res.status(500).send({ error: err.message });
+      }
+      // Notification inserted successfully, now call the external API
+      try {
+        const externalApiUrl = 'https://app.nativenotify.com/api/notification';
+        const externalApiPayload = {
+          appId: 16660,
+          appToken: 'Oia6wxWf6ZTJKSJBLlbU5V',
+          title: title,
+          body: message,
+          dateSent: new Date().toLocaleString(), // You might want to format this according to your needs
+          pushData: { yourProperty: 'yourPropertyValue' },
+          bigPictureURL: 'Big picture URL as a string',
+        };
+        const externalApiResponse = await axios.post(externalApiUrl, externalApiPayload);
+        // Handle the response from the external API
+        console.log('External API Response:', externalApiResponse.data);
         res.status(200).send({ message: 'Notification sent successfully' });
+      } catch (externalApiError) {
+        console.error('Error calling external API:', externalApiError);
+        res.status(500).send({ error: 'Error calling external API' });
       }
     });
   } catch (error) {
@@ -33,6 +51,7 @@ router.post('/send-notification', async (req, res) => {
     res.status(500).send({ error: 'Internal Server Error' });
   }
 });
+
 
 // Get notification endpoint
 router.get('/get-notification', (req, res) => {
@@ -88,10 +107,10 @@ router.get('/get-notification', (req, res) => {
 // Edit notification endpoint
 router.patch('/edit-notification/:NotificationId', (req, res) => {
   const { NotificationId } = req.params;
-  const { title, message, image, linkto } = req.body;
+  const { title, message, image, link } = req.body;
 
-  const updateSql = 'UPDATE notifications SET title=?, message=?, image=?, linkto=? WHERE NotificationId=?';
-  db.query(updateSql, [title, message, image, linkto, NotificationId], (err, result) => {
+  const updateSql = 'UPDATE notifications SET title=?, message=?, image=?, link=? WHERE NotificationId=?';
+  db.query(updateSql, [title, message, image, link, NotificationId], (err, result) => {
     if (err) {
       res.status(500).send({ error: err.message });
     } else {
@@ -105,21 +124,52 @@ router.patch('/edit-notification/:NotificationId', (req, res) => {
 });
 
 // Delete notification endpoint
-router.delete('/delete-notification/:NotificationId', (req, res) => {
-  const { NotificationId } = req.params;
-
-  const deleteSql = 'DELETE FROM notifications WHERE NotificationId=?';
-  db.query(deleteSql, [NotificationId], (err, result) => {
-    if (err) {
-      res.status(500).send({ error: err.message });
-    } else {
-      if (result.affectedRows === 0) {
-        res.status(404).send({ message: 'No matching notification found' });
-      } else {
-        res.status(200).send({ message: 'Notification deleted successfully' });
+router.delete('/delete-notification/:NotificationId', async (req, res) => {
+  try {
+    const { NotificationId } = req.params;
+    // Delete notification from local database
+    const deleteSql = 'DELETE FROM notifications WHERE NotificationId=?';
+    db.query(deleteSql, [NotificationId], async (err, result) => {
+      if (err) {
+        return res.status(500).send({ error: err.message });
       }
-    }
-  });
+      if (result.affectedRows === 0) {
+        return res.status(404).send({ message: 'No matching notification found' });
+      }
+      // Notification deleted successfully from local database, now delete from external API
+      try {
+        const externalApiUrl = `https://app.nativenotify.com/api/notification/inbox/notification/16660/Oia6wxWf6ZTJKSJBLlbU5V/${NotificationId}`;
+        const externalApiResponse = await axios.delete(externalApiUrl);
+        // Handle the response from the external API
+        console.log('External API Response:', externalApiResponse.data);
+        res.status(200).send({ message: 'Notification deleted successfully' });
+      } catch (externalApiError) {
+        console.error('Error calling external API:', externalApiError);
+        res.status(500).send({ error: 'Error calling external API' });
+      }
+    });
+  } catch (error) {
+    console.error('Error in delete-notification endpoint:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 });
+
+// // Delete notification endpoint
+// router.delete('/delete-notification/:NotificationId', (req, res) => {
+//   const { NotificationId } = req.params;
+
+//   const deleteSql = 'DELETE FROM notifications WHERE NotificationId=?';
+//   db.query(deleteSql, [NotificationId], (err, result) => {
+//     if (err) {
+//       res.status(500).send({ error: err.message });
+//     } else {
+//       if (result.affectedRows === 0) {
+//         res.status(404).send({ message: 'No matching notification found' });
+//       } else {
+//         res.status(200).send({ message: 'Notification deleted successfully' });
+//       }
+//     }
+//   });
+// });
 
 module.exports = router;
