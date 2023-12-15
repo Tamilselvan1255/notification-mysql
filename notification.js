95adb66b41d5ff193bc2ee002f38b8058420b0b9
anbushan <132537158+anbushan@users.noreply.github.com>
2023-12-15 12:03:53 +0000

    fixed the notification issue

--- notification.js
+++ notification.js
@@ -213,8 +213,8 @@ router.delete('/delete-push-notification/:refer_notification_id', async (req, re
   const { refer_notification_id } = req.params;
   try {
     // Delete the external notification
-    const externalApiUrl = `https://app.nativenotify.com/api/notification/inbox/notification/16351/hYNQ78ihflsQqOQA5RhYBN/${refer_notification_id}`;
-    await axios.delete(externalApiUrl);
+    // const externalApiUrl = `https://app.nativenotify.com/api/notification/inbox/notification/16351/hYNQ78ihflsQqOQA5RhYBN/${refer_notification_id}`;
+    // await axios.delete(externalApiUrl);
 
     // Delete the internal notification
     const deleteSql = 'DELETE FROM notifications WHERE refer_notification_id=?';
@@ -230,6 +230,115 @@ router.delete('/delete-push-notification/:refer_notification_id', async (req, re
     res.status(500).send({ error: 'Internal Server Error' });
   }
 });
+
+
+
+router.post('/send-push-notification-users', upload.single('image'), async (req, res) => {
+  try {
+    const { title, message, link } = req.body;
+
+    if (!title || !message || !link) {
+      return res.status(400).send({ error: 'Please enter all entities' });
+    }
+
+    if (!isValidUrl(link)) {
+      return res.status(400).send({ error: 'Invalid URL link' });
+    }
+
+    if (!req.file) {
+      return res.status(400).send({ error: 'No file uploaded' });
+    }
+
+    const file = req.file;
+    const fileName = file.originalname;
+    const filePath = file.path;
+
+    const s3Params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: 'uploads/' + fileName,
+      Body: require('fs').createReadStream(filePath),
+      ACL: 'public-read',
+      ContentType: file.mimetype,
+    };
+
+    try {
+      const s3Response = await s3.upload(s3Params).promise();
+      const imageUrl = s3Response.Location;
+
+      console.log('Image uploaded to S3:', imageUrl);
+
+      // Insert notification into local database
+      const insertSql = 'INSERT INTO notifications (title, message, image, link) VALUES (?, ?, ?, ?)';
+      db.query(insertSql, [title, message, imageUrl, link], async (err, result) => {
+        if (err) {
+          console.error('Error in send-push-notification endpoint:', err);
+          return res.status(500).send({ error: err.message });
+        }
+
+        // Retrieve Expo push tokens from the user table
+        const userTokensSql = 'SELECT token FROM users';
+        db.query(userTokensSql, async (tokenErr, tokenResults) => {
+          if (tokenErr) {
+            console.error('Error retrieving user tokens:', tokenErr);
+            return res.status(500).send({ error: tokenErr.message });
+          }
+
+          const expoTokens = tokenResults.map((row) => row.token);
+
+          // Notification inserted successfully, now call the external API
+          try {
+            const externalApiUrl = 'https://exp.host/--/api/v2/push/send';
+            const externalApiPayload = {
+              to: expoTokens,
+              title: title,
+              body: message,
+              sound: 'default',
+            };
+
+            const externalApiResponse = await axios.post(externalApiUrl, externalApiPayload);
+
+            // Handle the response from the external API
+            console.log('External API Response:', externalApiResponse.data);
+
+            // Check if the response status is 200
+            if (externalApiResponse.data) {
+              // Assuming the response is an array and you want to get the first element
+              const firstNotificationId = externalApiResponse?.data?.id;
+
+              // Insert the notification_id into your local database
+              const updateSql = 'UPDATE notifications SET refer_notification_id = ? WHERE NotificationId = ?';
+              db.query(updateSql, [firstNotificationId, result.insertId], (updateErr) => {
+                if (updateErr) {
+                  console.error('Error updating refer_notification_id:', updateErr);
+                }
+              });
+
+              res.status(200).send({ message: 'Notification sent successfully' });
+            } else {
+              res.status(500).send({ error: 'First external API call failed' });
+            }
+          } catch (externalApiError) {
+            console.error('Error calling external API:', externalApiError);
+            res.status(500).send({ error: 'Error calling external API' });
+          }
+        });
+      });
+    } catch (s3Error) {
+      console.error('Error uploading image to S3:', s3Error);
+      res.status(500).send({ error: 'Error uploading image to S3' });
+    } finally {
+      require('fs').unlinkSync(filePath);
+    }
+  } catch (error) {
+    console.error('Error in send-push-notification endpoint:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+
+
 
 // Helper function to execute SQL queries with promises
 function queryAsyncFunc(sql, params) {
